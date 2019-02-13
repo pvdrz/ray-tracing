d3c7a564229c4977afd44512d3218a83cd6dd2e2
Christian Poveda <christianpoveda@protonmail.com>
2019-02-13 12:49:56 -0500

    Add camera struct

---
+++ src/camera.rs
@@ -0,0 +1,28 @@
+use crate::ray::Ray;
+use crate::vec3::Vec3;
+use crate::Num;
+
+pub struct Camera {
+    lower_left_corner: Vec3,
+    horizontal: Vec3,
+    vertical: Vec3,
+    origin: Vec3,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        Camera {
+            lower_left_corner: Vec3::new(-2.0, -1.0, -1.0),
+            horizontal: Vec3::new(4.0, 0.0, 0.0),
+            vertical: Vec3::new(0.0, 2.0, 0.0),
+            origin: Vec3::new(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub fn get_ray(&self, u: Num, v: Num) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
+}
